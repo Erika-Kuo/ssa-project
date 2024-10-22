8cd172b65fc220de9eef322bd83d4d93a4c1e1b9
Erika Kuo <erika.kuo@education.nsw.gov.au>
2024-10-22 15:03:54 +1100

    Working version, fixed the previously mentioned html files. Completed entirety of week 2's user registration. Added user: OneLonelyBraincell, hopefully I can remember the password, hint: It's my username, and created an admin user under erika.kuo with the same password

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'first_name', 'surname', 'nickname')
+    search_fields = ('user__username', 'nickname')
+    list_filter = ('nickname',)
+
+admin.site.register(Profile, ProfileAdmin)
