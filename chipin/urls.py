f9e56a80f017d7afbe76ce01435c5fc793c7321c
Erika Kuo <erika.kuo@education.nsw.gov.au>
2024-11-05 15:18:10 +1100

    Can accept invites now!!!!

--- chipin/urls.py
+++ chipin/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
    path('group/<int:group_id>/', views.group_detail, name='group_detail'),
    path('group/<int:group_id>/invite/', views.invite_users, name='invite_users'),
    path('group/<int:group_id>/delete/', views.delete_group, name='delete_group'),
+   path('group/<int:group_id>/accept/', views.accept_invite, name='accept_invite'),
 ]
