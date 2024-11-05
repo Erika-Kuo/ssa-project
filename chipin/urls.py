0dd417a85b8e76a67795b0f3e85351f95da71c5e
Erika Kuo <erika.kuo@education.nsw.gov.au>
2024-11-05 14:01:34 +1100

    Group creation now KINDA works but it needs to be fixed a little bit.

--- chipin/urls.py
+++ chipin/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from . import views
-
 urlpatterns = [
    path("", views.home, name="home"),
    path('create_group/', views.create_group, name='create_group'),
-   path('group//', views.group_detail, name='group_detail'),
-   path('group//invite/', views.invite_users, name='invite_users'),
-   path('group//delete/', views.delete_group, name='delete_group'),
+   path('group/<int:group_id>/', views.group_detail, name='group_detail'),
+   path('group/<int:group_id>/invite/', views.invite_users, name='invite_users'),
+   path('group/<int:group_id>/delete/', views.delete_group, name='delete_group'),
 ]
