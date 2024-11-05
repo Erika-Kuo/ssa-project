3daf44398080dfa30491f794dda69769c5ffb2a4
Erika Kuo <erika.kuo@education.nsw.gov.au>
2024-11-05 15:05:09 +1100

    Invite users now works. However you can't exactly accept the invites which is very sad.

--- chipin/models.py
+++ chipin/models.py
@@ -3,8 +3,9 @@ from django.contrib.auth.models import User
 
 class Group(models.Model):
     name = models.CharField(max_length=100)
-    admin = models.ForeignKey(User, related_name='admin_groups', on_delete=models.CASCADE)
+    admin = models.ForeignKey(User, on_delete=models.CASCADE, related_name='admin_groups')
     members = models.ManyToManyField(User, related_name='group_memberships', blank=True)
+    invited_users = models.ManyToManyField(User, related_name='pending_invitations', blank=True)
 
     def __str__(self):
         return self.name
--- chipin/views.py
+++ chipin/views.py
@@ -10,8 +10,10 @@ from .forms import GroupCreationForm
 from .models import Group
 import urllib.parse
 
+@login_required
 def home(request):
-    return render(request, "chipin/home.html")
+    pending_invitations = Group.objects.filter(invited_users__email=request.user.email)
+    return render(request, "chipin/home.html", {'pending_invitations': pending_invitations})
 
 @login_required
 def create_group(request):
@@ -45,8 +47,33 @@ def invite_users(request, group_id):
     group = get_object_or_404(Group, id=group_id)
     users_not_in_group = User.objects.exclude(id__in=group.members.values_list('id', flat=True))
     if request.method == 'POST':
-        email = request.POST.get('email')
-        send_invitation_email(group, email, request)
-        messages.success(request, f'Invitation sent to {email}')
-        return redirect('chipin:group_detail', group_id=group.id)
-    return render(request, 'chipin/invite_users.html', {'group': group, 'users_not_in_group': users_not_in_group})
+        user_id = request.POST.get('user_id')
+        invited_user = get_object_or_404(User, id=user_id)      
+        if invited_user in group.invited_users.all():
+            messages.info(request, f'{invited_user.username} has already been invited.')
+        else:
+            group.invited_users.add(invited_user)
+            messages.success(request, f'Invitation sent to {invited_user.username}.')
+        return redirect('chipin:group_detail', group_id=group.id)  
+    return render(request, 'chipin/invite_users.html', {
+        'group': group,
+        'users_not_in_group': users_not_in_group
+    })
+
+@login_required
+def accept_invite(request, group_id):
+    group = get_object_or_404(Group, id=group_id)
+    user_id = request.GET.get('user_id')
+    if user_id:
+        invited_user = get_object_or_404(User, id=user_id)
+        if invited_user in group.members.all():
+            messages.info(request, f'{invited_user.username} is already a member of the group "{group.name}".')
+        elif invited_user in group.invited_users.all():
+            group.members.add(invited_user)
+            group.invited_users.remove(invited_user)  # Remove from invited list
+            messages.success(request, f'{invited_user.username} has successfully joined the group "{group.name}".')
+        else:
+            messages.error(request, "You are not invited to join this group.")
+    else:
+        messages.error(request, "Invalid invitation link.")  
+    return redirect('chipin:group_detail', group_id=group.id)
